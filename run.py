6bc62ab93856722cf2823778b235861d8000462d
Valentin <mail@valteu.com>
2024-07-23 00:17:03 +0200

    updated run.py for saving results in home

--- run.py
+++ run.py
@@ -3,21 +3,24 @@ import subprocess
 import shutil
 
 # Path to the datasets directory
-datasets_path = 'datasets_colmap_2'
+datasets_path = 'datasets'
 
-# Iterate over each directory inside the datasets folder
+# Output directory path
+output_root = 'output'
+
+# Process each directory inside the datasets folder
 for filename in os.listdir(datasets_path):
     dir_path = os.path.join(datasets_path, filename)
     
     # Check if the path is a directory
     if os.path.isdir(dir_path):
         try:
-            # Convert using Xvfb
+            # Convert using Xvfb (uncomment if needed)
             # print(f"Processing: {filename} - convert")
             # subprocess.run(['python3', 'convert.py', '-s', dir_path], check=True)
             
             # Train
-            output_dir = os.path.join('output', filename)
+            output_dir = os.path.join(output_root, filename)
             print(f"Processing: {filename} - train")
             subprocess.run(['python3', 'train.py', '-s', dir_path, '--eval', '-m', output_dir], check=True)
             
@@ -33,3 +36,20 @@ for filename in os.listdir(datasets_path):
             print(f"File not found error while processing {filename}: {e}")
         except Exception as e:
             print(f"An unexpected error occurred while processing {filename}: {e}")
+
+# Copy the output directory to the specified location on the server
+def copy_output_directory(src, dest):
+    try:
+        if os.path.exists(dest):
+            shutil.rmtree(dest)
+        shutil.copytree(src, dest)
+        print(f"Successfully copied {src} to {dest}")
+    except Exception as e:
+        print(f"An error occurred while copying {src} to {dest}: {e}")
+
+# Paths
+src_output_dir = '/workspace/output'
+dest_output_dir = '/dhc/home/valentin.teutschbein/output'
+
+# Perform the copy operation
+copy_output_directory(src_output_dir, dest_output_dir)
